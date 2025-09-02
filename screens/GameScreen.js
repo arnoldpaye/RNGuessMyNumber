1f9a97ec1ddb74040afcc982ff29645289f07aa0
Arnold Paye <arnoldpayet@gmail.com>
2025-09-02 16:22:37 -0400

    refactor: restructure components folder and enhance GameScreen logic with NumberContainer

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
+function generateRandomNumberBetween(min, max, exclude) {
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  return randomNumber === exclude
+    ? generateRandomNumberBetween(min, max, exclude)
+    : randomNumber;
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomNumberBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
       </View>
